9d7dbe25e3b2dc50f6834346c0a1850079bd0cba
BestNameYet <samuel.john.nixon@gmail.com>
2019-06-15 07:45:18 -0500

    Refactored for readability

--- Solution.java
+++ Solution.java
@@ -8,39 +8,43 @@ public class Solution {
 	public static void main(String[] args) throws IOException{
 		System.out.println("Solution running...");
 		
+		long startTime = System.currentTimeMillis();
 		int result = 0;
 		int arraySize = 612;
 		int iterations = 1000000;
+		int randomIndex = 0;
+		Integer currentValue;
+		Integer nextValue;
 		ArrayList<Integer> baseArray = new ArrayList<Integer>(arraySize);
+		ArrayList<Integer> workingArray = new ArrayList<Integer>(baseArray.size());
 		Random randomIntegerGenerator = new Random();
 				
-		for(int i=0; i<arraySize;i++) {
+		for(int i=0; i<baseArray.size();i++) {
 			baseArray.add(new Integer(i));
 		} 
 		
 		
 		for(int i=0; i<iterations; i++) {
-			ArrayList<Integer> workingArray= new ArrayList<Integer>(baseArray.size());
-			workingArray.addAll(baseArray);
 			
-			int randomIndex = randomIntegerGenerator.nextInt(workingArray.size());
-			Integer formerValue = workingArray.get(randomIndex);
-			Integer latterValue = null;
+			workingArray.addAll(baseArray);
+			randomIndex = randomIntegerGenerator.nextInt(workingArray.size());
+			currentValue = workingArray.get(randomIndex);
+			nextValue = null;
 			workingArray.remove(randomIndex);
 					
 			while(workingArray.size()>0) {
-								
 				randomIndex = randomIntegerGenerator.nextInt(workingArray.size());
-				latterValue = workingArray.get(randomIndex);
+				nextValue = workingArray.get(randomIndex);
 				workingArray.remove(randomIndex);
 				
-				Integer diff = latterValue - formerValue;
+				Integer diff = nextValue - currentValue;
 				if(diff.equals(Integer.valueOf(1))) {
 					result = result + 1;
+					workingArray.clear();
 					break;
 				}
 				else {
-					formerValue = latterValue;
+					currentValue = nextValue;
 				}
 				
 			}
@@ -48,6 +52,6 @@ public class Solution {
 		
 		}
 		System.out.println("Array Size: "+arraySize);
-		System.out.println("Result of "+iterations+" interations: "+result);
+		System.out.println("Result of "+iterations+" interations in "+(System.currentTimeMillis()-startTime)+" ms: "+result);
 	}
 }
